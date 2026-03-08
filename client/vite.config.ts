995e518eceeee9615b655fc5c4d7b6d88e3745e7
jonpena <gran_200014@hotmail.com>
2026-03-08 12:23:32 +0100

    chore: install shadcn and config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/adient/',
-  server: {
-    port: 5173,
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
+  base: "/adient/",
 });
